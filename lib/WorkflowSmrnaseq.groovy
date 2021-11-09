0a652f6563cf893c660f404b0dc2283dfdd862a9
JoseEspinosa <kadomu@gmail.com>
2021-11-09 17:14:12 +0100

    Refactor initialise

--- lib/WorkflowSmrnaseq.groovy
+++ lib/WorkflowSmrnaseq.groovy
@@ -8,12 +8,10 @@ class WorkflowSmrnaseq {
     // Check and validate parameters
     //
     public static void initialise(params, log) {
-        genomeExistsError(params, log)
-
-        // if (!params.fasta) {
-        //     log.error "Genome fasta file not specified with e.g. '--fasta genome.fa' or via a detectable config file."
-        //     System.exit(1)
-        // }
+        public static void initialise(params, log) {
+        if (params.genomes && params.genome && !params.genomes.containsKey(params.genome)) {
+            genomeExistsError(params, log)
+        }
     }
 
     //
